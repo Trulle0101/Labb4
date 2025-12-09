7e72d4e5ad141ebbc33c16ca83cec069d989fb1b
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2025-12-09 12:55:04 +0100

    Först commit labb4, gjort main class

--- src/Labben4.java
+++ src/Labben4.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Scanner;
 
-public class Labben4 {
+public class Labben4
+{
+    public static void main(String[] args)
+    {
+        VocabTrainer trainer = new VocabTrainer();
+        trainer.run();
+        System.out.println("** Glosövning – Engelska **");
+        System.out.println("Skriv det engelska ordet. Avlsuta genom att skriva Q.\n");
+    }
 }
