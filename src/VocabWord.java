a5788a6df5ff5e27b4d404c3185f7085f2181f73
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-10 21:43:28 +0100

    Fjärde commit i labb4. Skapade klassen VocabWord

---
+++ src/VocabWord.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class VocabWord
+{
+    private final String swedishWord;
+    private final List<String> englishTranslations;
+
+    public VocabWord(String swedishWord, List<String> englishTranslations)
+    {
+        this.swedishWord = swedishWord;
+        this.englishTranslations = englishTranslations;
+    }
+
+
+}
