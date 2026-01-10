bebdbe2e0b482effa735fcfd466caebc4c4f3d89
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-10 21:57:30 +0100

    Femte commit i labb4. Skapade klassen AnswerResult

---
+++ src/AnswerResult.java
@@ -0,0 +1,10 @@
+
+public class AnswerResult
+{
+
+    public static final AnswerResult CORRECT = new AnswerResult("CORRECT");
+    public static final AnswerResult ALMOST  = new AnswerResult("ALMOST");
+    public static final AnswerResult WRONG   = new AnswerResult("WRONG");
+    public static final AnswerResult QUIT    = new AnswerResult("QUIT");
+
+}
