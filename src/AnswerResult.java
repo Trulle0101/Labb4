6cff67e00203e6d94c879cfe103066a1ee673597
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-11 22:51:04 +0100

    Elfte commit i labb4. Ändrade klassen AnswerResult

--- src/AnswerResult.java
+++ src/AnswerResult.java
@@ -2,9 +2,9 @@
 public class AnswerResult
 {
 
-    public static final AnswerResult CORRECT = new AnswerResult("CORRECT");
-    public static final AnswerResult ALMOST  = new AnswerResult("ALMOST");
-    public static final AnswerResult WRONG   = new AnswerResult("WRONG");
-    public static final AnswerResult QUIT    = new AnswerResult("QUIT");
+    public static final AnswerResult CORRECT = new AnswerResult();
+    public static final AnswerResult ALMOST  = new AnswerResult();
+    public static final AnswerResult WRONG   = new AnswerResult();
+    public static final AnswerResult QUIT    = new AnswerResult();
 
 }
