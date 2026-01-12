197afd62974e59b87abf7c7e1f814cd9c84fe2ae
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-12 21:25:18 +0100

    Femtonde commit i Labb4. Kommenterade klassen VocabWord enligt JavaDoc.

--- src/VocabWord.java
+++ src/VocabWord.java
@@ -1,21 +1,44 @@
 import java.util.List;
 
+/**
+ * Klassen {@code VocabWord} representerar ett glosord i programmet.
+ * <p>
+ * Glosord består av ett svenskt ord och engelsk översättning.
+ */
 public class VocabWord
 {
+    /** Det svenska ordet. */
     private final String swedishWord;
+    /** Lista med engelsk översättning till de svenska orden. */
     private final List<String> englishTranslations;
 
+    /**
+     * Skapar ett nytt {@code VocabWord}-objekt.
+     *
+     * @param swedishWord det svenska ordet
+     * @param englishTranslations en lista med engelska översättningar.
+     */
     public VocabWord(String swedishWord, List<String> englishTranslations)
     {
         this.swedishWord = swedishWord;
         this.englishTranslations = englishTranslations;
     }
 
+    /**
+     * Returnerar det svenska ordet.
+     *
+     * @return det svenska ordet.
+     */
     public String getSwedishWord()
     {
         return swedishWord;
     }
 
+    /**
+     * Returnerar en lista med engelska översättningar.
+     *
+     * @return en lista med engelska översättningar.
+     */
     public List<String> getEnglishTranslations()
     {
         return englishTranslations;
