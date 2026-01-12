9a9fad10ff6c924d55c4219938acb8e59a011e08
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-12 21:42:59 +0100

    Sjuttonde commit i Labb4. Kommenterade klassen AnswerResult enligt JavaDoc.

--- src/AnswerResult.java
+++ src/AnswerResult.java
@@ -1,10 +1,21 @@
 
+/**
+ * Klassen {@code AnswerResult} representerar de möjliga resultat
+ * av en utvärdering av användarens svar i glosövningsprogrammet.
+ * <p>
+ * Klassen fungerar innehåller fördefinierade instanser
+ * som beskriver olika typer av svar.
+ */
 public class AnswerResult
 {
 
+    /** Indikerar att användarens svar är helt korrekt. */
     public static final AnswerResult CORRECT = new AnswerResult();
+    /** Indikerar att användarens svar är nästan korrekt. */
     public static final AnswerResult ALMOST  = new AnswerResult();
+    /** Indikerar att användarens svar är felaktigt. */
     public static final AnswerResult WRONG   = new AnswerResult();
+    /** Indikerar att användaren valt att avsluta programmet. */
     public static final AnswerResult QUIT    = new AnswerResult();
 
 }
