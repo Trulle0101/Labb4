c90677ee44245533b220899de8023e1526f437db
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-12 21:11:55 +0100

    Fjortonde commit i Labb4. Kommenterade klassen VocabTrainer enligt JavaDoc

--- src/VocabTrainer.java
+++ src/VocabTrainer.java
@@ -2,19 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Huvudlassen {@code VocabTrainer} ansvarar för att köra ett
+ * glosförhör i engelska.
+ * <p>
+ * Programmet visar ett svenskt ord och användaren ska skriva
+ * motsvarande engelska ord. Resultatet bedöms och statistik
+ * visas löpande tills programmet är slut.
+ */
 public class VocabTrainer
 {
-
+    /** Lista med glosor som används i övningsprogrammet**/
     private List<VocabWord> vocabWords;
+    /** Skanner för att läsa in användarens input**/
     private Scanner scanner;
+    /** Maximalt antal glosor som ställs under övningsprogrammet**/
     private int maxQuestions = 10;
 
+    /**
+     * Skapar ett nytt {@code VocabTrainer}-objekt.
+     * <p>
+     * Initierar scanner samt laddar en uppsättning av glosor.
+     */
     public VocabTrainer()
     {
         this.scanner = new Scanner(System.in);
         this.vocabWords = createDefaultVocabulary();
     }
 
+    /**
+     * Startar och kör glosövningen.
+     * <p>
+     * Metoden itererar igenom gloslistan, tar emot användarens svar,
+     * utvärderar svarer och visar feedback. Programmet avslutas om
+     * användaren skriver {@code Q} eller när max antal frågor har ställts.
+     */
     public void run()
     {
         System.out.println("** GLOSÖVNING - ENGELSKA **");
@@ -64,6 +86,11 @@ public class VocabTrainer
         System.out.println("Du svarade på totalt " + asked + " glosor och hade " + correctCount + " rätt. Välkommen åter!");
     }
 
+    /**
+     * Skapar och returnerar en standardlista med svenska och engelska glosor.
+     *
+     * @return en {@link List} med {@link VocabWord}-objekt
+     */
     private List<VocabWord> createDefaultVocabulary() {
         List<VocabWord> list = new ArrayList<>();
 
